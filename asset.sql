6e051e7003467348f39a1c60d24cb7fee2d948c6
carolkyyy <2751476228@qq.com>
2025-07-28 16:19:58 +0800

    add insert

--- asset.sql
+++ asset.sql
@@ -10,3 +10,6 @@ CREATE TABLE Asset (
     stock_name VARCHAR(255),
     FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
+
+INSERT INTO Asset (account_id, type, create_time, update_time, price, quantity, stock_ticker, stock_name)
+VALUES (1, 0, NOW(), NOW(), 100.50, 20, 'AAPL', 'Apple Inc.');
